e985a814bb2c19655b1393da44eabfacb6bda814
GabrielGil-Castro <gsgc05@gmail.com>
2025-03-31 19:22:33 -0300

    Adicionando home e animações

--- my-portifolio/src/components/Navbar.jsx
+++ my-portifolio/src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-     <nav className='bg-blue-600 p-4 text-white flex justify-around items-center'>
+     <nav className='bg-blue-600 p-2 text-white flex justify-around items-center'>
         <h1 className='text-xl font-bold'>Minha NavBar</h1>
         <ul>
             <li><Link to='/' className='hover:underline'>HOME</Link></li>
             <li><Link to='/projects' className='hover:underline'>PROJETOS</Link></li>
             <li><Link to='/contact' className='hover:underline'>CONTATO</Link></li>
+            <li><Link to='/about' className='hover:underline'>SOBRE</Link></li>
         </ul>
 
      </nav>
--- my-portifolio/src/pages/About.jsx
+++ my-portifolio/src/pages/About.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
-import '../App.css'
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
-export default function About() {
+const AnimationExample = () => {
+  const [animate, setAnimate] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  const handleClick = () => {
+    setAnimate(!animate);
+    setButtonClicked(true); // Esconde o botão após o clique
+  };
+
   return (
-    <div class= 'bg-gray-500'>About</div>
-  )
-}
+    <div
+      className={`h-screen flex justify-center items-center transition duration-200 ease-out ${animate ? "bg-black text-white" : ""}`}
+    >
+      <div>
+        {!buttonClicked && (
+          <motion.div
+            className="px-4 py-2 rounded bg-red-500 text-white hover:bg-red-400 text-xl cursor-pointer select-none transition transform duration-500"
+            onClick={handleClick}
+            animate={{
+              scale: animate ? 1.5 : 1,
+              y: animate ? -16 : 0,
+            }}
+          >
+            Clique aqui para saber mais
+          </motion.div>
+        )}
+        
+        <motion.div
+          className="text-center mt-4 font-bold text-2xl transition ease-in"
+          initial={{ opacity: 0, y: 8 }}
+          animate={{
+            opacity: animate ? 1 : 0,
+            y: animate ? 0 : 8,
+          }}
+          transition={{ duration: animate ? 1 : 0.2 }}
+        >
+          {buttonClicked ? (
+            <Link to="/" className="text-blue-500 hover:text-blue-700">
+              Ir para a Home
+            </Link>
+          ) : (
+            "Hello World"
+          )}
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimationExample;
